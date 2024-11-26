4664827c46a676677c00130d89519467676534ce
Olivier Louvignes <olivier@mgcrea.io>
2024-11-26 12:04:52 +0100

    feat(update): minor changes

--- src/verify.ts
+++ src/verify.ts
@@ -3,34 +3,36 @@ import { createVerify } from "node:crypto";
 import { jwtClaimsSchema, jwtHeaderSchema } from "src/schemas";
 import { decodeBase64Url, getKeyById, JWTVerifyError } from "src/utils";
 
-export const JWK_TO_ALG = {
-  RSA: "RSA-SHA256",
-};
-
 type VerifyTokenOptions =
   | {
       token: string;
+      alg?: string;
       jwksUri?: string;
       tenantId: string;
       clientId?: string;
     }
   | {
       token: string;
+      alg?: string;
       jwksUri: string;
       tenantId?: string;
       clientId?: string;
     };
 
 // @docs https://learn.microsoft.com/en-us/entra/identity-platform/id-token-claims-reference#payload-claims
-export const verifyToken = async ({ token, tenantId, clientId, ...options }: VerifyTokenOptions) => {
+export const verifyToken = async ({
+  token,
+  tenantId,
+  clientId,
+  alg = "RSA-SHA256",
+  ...options
+}: VerifyTokenOptions) => {
   const jwksUri = options.jwksUri ?? `https://login.microsoftonline.com/${tenantId}/discovery/v2.0/keys`;
   const tokenParts = token.split(".");
   assert(tokenParts.length === 3 && tokenParts[0], "Invalid token format");
   const header = jwtHeaderSchema.parse(JSON.parse(decodeBase64Url(tokenParts[0]).toString()));
   const key = await getKeyById(jwksUri, header.kid);
   const signature = decodeBase64Url(tokenParts[2]);
-  assert(key.kty in JWK_TO_ALG, `Unsupported key type ${key.kty}`);
-  const alg = JWK_TO_ALG[key.kty as keyof typeof JWK_TO_ALG];
   const verify = createVerify(alg);
   verify.update(`${tokenParts[0]}.${tokenParts[1]}`);
   const result = verify.verify({ key, format: "jwk" }, signature);
